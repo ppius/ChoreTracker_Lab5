5936bfef192640d5ab2a71757671da48934efbe0
Peace Pius <peacepius716@gmail.com>
2019-02-12 20:47:14 -0500

    Passed all tests for task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,15 @@
 class Task < ApplicationRecord
+
+    #Relationships
     has_many :chores
     has_many :children, through: :chores
+
+    #Validations
+    validates_presence_of :name
+    validates_numericality_of :points, :only_integer => true, :greater_than => 0
+
+    #Scopes
+    scope :alphabetical, -> { order('name')}
+    scope :active,       -> { where(active: true)}
+
 end

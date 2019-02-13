edc4d4af5cd472b189fe2cdfba0917d2a371efb7
Peace Pius <peacepius716@gmail.com>
2019-02-12 20:05:31 -0500

    Added validation tests for child test

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -4,7 +4,10 @@ class ChildTest < ActiveSupport::TestCase
   # TODO: Relationship macros
   should have_many(:chores)
   should have_many(:tasks).through(:chores)
+  
   # TODO: Validation macros
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
 
   # TODO: Context testing
 

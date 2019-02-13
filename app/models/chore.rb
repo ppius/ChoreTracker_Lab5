3240df4f601c3a9dc383a93c52ef99d8ddf08a4e
Peace Pius <peacepius716@gmail.com>
2019-02-12 21:28:32 -0500

    Passed all the tests for chore_test

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,5 +1,28 @@
 class Chore < ApplicationRecord
+    
     #Relationship
     belongs_to :child
     belongs_to :task
+
+    #Validations
+    validates_presence_of :child_id, :task_id
+    validates_date        :due_on
+
+    #Scopes
+    scope :by_task,        -> {joins(:task).order('name')}
+    scope :chronological,  -> { order('due_on, completed')}
+    scope :pending,        -> { where(completed: false)}
+    scope :done,           -> { where(completed: true)}
+    scope :upcoming,       -> { where('due_on >= ?',  Date.current)}
+    scope :past,           -> { where('due_on < ?',  Date.current)}
+
+    #Methods
+    def status
+        if self.completed == true
+            return 'Completed'
+        else
+            return 'Pending'
+        end
+    end
+
 end
